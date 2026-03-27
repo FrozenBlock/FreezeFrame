986e397396e348375608a24edd0b3bb85266f884
AViewFromTheTop <AViewFromTheTop@users.noreply.github.com>
2026-03-27 00:50:30 +0000

    Apply automatic changes

--- src/main/java/net/frozenblock/freezeframe/config/gui/FFConfigGuiHelper.java
+++ src/main/java/net/frozenblock/freezeframe/config/gui/FFConfigGuiHelper.java
@@ -1,6 +1,6 @@
 /*
  * Copyright 2026 FrozenBlock
- * This file is part of The Copperier Age.
+ * This file is part of Freeze Frame.
  *
  * This program is free software; you can modify it under
  * the terms of version 1 of the FrozenBlock Modding Oasis License
