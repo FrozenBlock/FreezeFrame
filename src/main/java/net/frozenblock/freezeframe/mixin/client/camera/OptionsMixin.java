09279f48e1920aaa0dd3a0cd0866b5eb8e7d3a00
AViewFromTheTop <87103914+AViewFromTheTop@users.noreply.github.com>
2026-06-08 21:32:39 -0400

    fix camera type in screenshot

---
+++ src/main/java/net/frozenblock/freezeframe/mixin/client/camera/OptionsMixin.java
@@ -0,0 +1,38 @@
+/*
+ * Copyright 2026 FrozenBlock
+ * This file is part of Freeze Frame.
+ *
+ * This program is free software; you can modify it under
+ * the terms of version 1 of the FrozenBlock Modding Oasis License
+ * as published by FrozenBlock Modding Oasis.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * FrozenBlock Modding Oasis License for more details.
+ *
+ * You should have received a copy of the FrozenBlock Modding Oasis License
+ * along with this program; if not, see <https://github.com/FrozenBlock/Licenses>.
+ */
+
+package net.frozenblock.freezeframe.mixin.client.camera;
+
+import com.llamalad7.mixinextras.injector.ModifyReturnValue;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+import net.frozenblock.freezeframe.client.screenshot.FFScreenshotUtil;
+import net.minecraft.client.CameraType;
+import net.minecraft.client.Options;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+
+@Environment(EnvType.CLIENT)
+@Mixin(Options.class)
+public class OptionsMixin {
+
+	@ModifyReturnValue(method = "getCameraType", at = @At("RETURN"))
+	public CameraType freezeFrame$ignoreCameraTypeWhileScreenshotting(CameraType original) {
+		if (!FFScreenshotUtil.screenshotting()) return original;
+		return CameraType.FIRST_PERSON;
+	}
+}
