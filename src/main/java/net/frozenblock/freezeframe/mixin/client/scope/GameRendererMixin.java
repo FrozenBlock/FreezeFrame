3f545ee21c11c06d86689bfa958817f81db24e70
AViewFromTheTop <87103914+AViewFromTheTop@users.noreply.github.com>
2026-06-09 13:26:55 -0400

    Update GameRendererMixin.java

--- src/main/java/net/frozenblock/freezeframe/mixin/client/scope/GameRendererMixin.java
+++ src/main/java/net/frozenblock/freezeframe/mixin/client/scope/GameRendererMixin.java
@@ -26,6 +26,7 @@ import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.GameRenderer;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.player.Player;
+import org.objectweb.asm.Opcodes;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
@@ -44,8 +45,9 @@ public class GameRendererMixin {
 	@Inject(
 		method = "shouldRenderBlockOutline",
 		at = @At(
-			value = "INVOKE",
-			target = "Lnet/minecraft/world/entity/LivingEntity;getMainHandItem()Lnet/minecraft/world/item/ItemStack;"
+			value = "FIELD",
+			target = "Lnet/minecraft/client/Options;hideGui:Z",
+			opcode = Opcodes.GETFIELD
 		),
 		cancellable = true
 	)
